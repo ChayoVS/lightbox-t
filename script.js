e8a26d6f6a4952a024b59c05c704c79020bf69e4
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-05-17 10:30:15 -0500

    Añadiendo librería magnific popup, estilos con bootstrap, responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+(function($) {
+    $('.popup').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+})(jQuery);
